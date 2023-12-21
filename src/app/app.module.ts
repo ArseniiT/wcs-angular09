2d74e97e0416e9b5cef9c5fc6db67d6d3607fe9a
arseniit <arseniit@gmail.com>
2023-12-21 15:38:20 +0100

    Compile basic website skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateKittenComponent } from './create-kitten/create-kitten.component';
+import { ListKittenComponent } from './list-kitten/list-kitten.component';
+import { UserKittenComponent } from './user-kitten/user-kitten.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateKittenComponent,
+    ListKittenComponent,
+    UserKittenComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/cat.model.ts
@@ -0,0 +1,8 @@
+export class Cat {
+    constructor(
+        public nom: string,
+        public race: string,
+        public birthday: Date,
+        public imgUrl: string
+    ){}
+}
